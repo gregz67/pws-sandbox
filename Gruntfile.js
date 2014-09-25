826197f66d7fc59c9c353e397fc4fd06b7fa3e79
GregZ <ziebold@gmail.com>
2014-09-25 17:34:46 -0600

    simple build & deploy

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+  });
+
+  // Default task.
+  grunt.registerTask('default', []);
+};
